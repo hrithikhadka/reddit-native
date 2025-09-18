ad513df48fe8ddc85ec8c08a3b8aad1e0c42fd04
hrithikhadka <hrithikhadka00@gmail.com>
2025-09-18 21:12:44 +0300

    add custom header title and icon for home tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,20 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { Tabs } from "expo-router";
+
 export default function _layout() {
-  return <Tabs />;
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "black" }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerTitle: "Reddit",
+          headerTintColor: "#FF5700",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }

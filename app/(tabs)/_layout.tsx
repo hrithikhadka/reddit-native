a7c8f98a670b2d6cb195a36bc849cee09617e4bc
hrithikhadka <hrithikhadka00@gmail.com>
2025-09-20 13:58:22 +0300

    fix chat icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { AntDesign, Feather } from "@expo/vector-icons";
+import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function _layout() {
@@ -17,11 +17,33 @@ export default function _layout() {
       />
 
       <Tabs.Screen
+        name="communities"
+        options={{
+          title: "Communities",
+          tabBarIcon: ({ color }) => (
+            <Feather name="users" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="plus" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="chat"
         options={{
           title: "Chat",
           tabBarIcon: ({ color }) => (
-            <Feather name="users" size={24} color={color} />
+            <Ionicons
+              name="chatbubble-ellipses-outline"
+              size={24}
+              color={color}
+            />
           ),
         }}
       />
@@ -32,26 +54,6 @@ export default function _layout() {
           title: "Inbox",
           tabBarIcon: ({ color }) => (
             <Feather name="bell" size={24} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="create"
-        options={{
-          title: "Create",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="plus" size={24} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="communities"
-        options={{
-          title: "Communities",
-          tabBarIcon: ({ color }) => (
-            <Feather name="users" size={24} color={color} />
           ),
         }}
       />

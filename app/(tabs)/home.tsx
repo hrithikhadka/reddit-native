a6633865b452e0efe913744e6be40a2d7c7f3667
hrithikhadka <hrithikhadka00@gmail.com>
2025-09-21 23:58:23 +0300

    first custom component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,8 +1,36 @@
-import { Text, View } from "react-native";
+import { formatDistanceToNowStrict } from "date-fns";
+import { Image, Text, View } from "react-native";
+import posts from "../../assets/data/posts.json";
+
 export default function Home() {
+  const post = posts[0];
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ paddingHorizontal: 15, paddingVertical: 10 }}>
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Image
+          source={{ uri: post.group.image }}
+          style={{ width: 20, height: 20, borderRadius: 10 }}
+        />
+        <Text style={{ fontWeight: "bold" }}>{post.group.name}</Text>
+        <Text style={{ color: "grey" }}>
+          {formatDistanceToNowStrict(new Date(post.created_at))}
+        </Text>
+
+        <View style={{ marginLeft: "auto" }}>
+          <Text
+            style={{
+              backgroundColor: "#0d469b",
+              color: "white",
+              paddingVertical: 2,
+              paddingHorizontal: 7,
+              borderRadius: 10,
+              fontWeight: "bold",
+            }}
+          >
+            Join
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }

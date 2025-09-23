1061a5aec3b44e892bf46b89ee46790e8472862d
hrithikhadka <hrithikhadka00@gmail.com>
2025-09-23 12:46:33 +0300

    style footer icons and conditional rendering image and description

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -22,42 +22,70 @@ export default function Home() {
 
       {/*Post Body*/}
       <Text style={styles.title}>{post.title}</Text>
-      <Image
-        source={{ uri: post.image }}
-        style={{ width: "100%", aspectRatio: 4 / 3, borderRadius: 15 }}
-      />
-      <Text numberOfLines={4}>{post.description}</Text>
+      {post.image && (
+        <Image
+          source={{ uri: post.image }}
+          style={{ width: "100%", aspectRatio: 4 / 3, borderRadius: 15 }}
+        />
+      )}
+      {!post.image && post.description && (
+        <Text numberOfLines={4}>{post.description}</Text>
+      )}
 
       {/*Post Footer*/}
       <View style={{ flexDirection: "row" }}>
-        <MaterialCommunityIcons
-          name="arrow-up-bold-outline"
-          size={19}
-          color="black"
-        />
-        <Text>{post.upvotes}</Text>
-        <MaterialCommunityIcons
-          name="arrow-down-bold-outline"
-          size={19}
-          color="black"
-        />
-        <MaterialCommunityIcons
-          name="comment-outline"
-          size={19}
-          color="black"
-        />
-        <Text>{post.nr_of_comments}</Text>
-
-        <View style={{ flexDirection: "row", marginLeft: "auto", gap: 10 }}>
+        <View style={{ flexDirection: "row", gap: 10 }}>
+          <View style={[{ flexDirection: "row" }, styles.iconBox]}>
+            <MaterialCommunityIcons
+              name="arrow-up-bold-outline"
+              size={19}
+              color="black"
+            />
+            <Text
+              style={{ fontWeight: "500", marginLeft: 5, alignSelf: "center" }}
+            >
+              {post.upvotes}
+            </Text>
+            <View
+              style={{
+                width: 1,
+                backgroundColor: "#D4D4D4",
+                height: 14,
+                marginHorizontal: 7,
+                alignSelf: "center",
+              }}
+            />
+            <MaterialCommunityIcons
+              name="arrow-down-bold-outline"
+              size={19}
+              color="black"
+            />
+          </View>
+          <View style={[{ flexDirection: "row" }, styles.iconBox]}>
+            <MaterialCommunityIcons
+              name="comment-outline"
+              size={19}
+              color="black"
+            />
+            <Text
+              style={{ fontWeight: "500", marginLeft: 5, alignSelf: "center" }}
+            >
+              {post.nr_of_comments}
+            </Text>
+          </View>
+        </View>
+        <View style={{ marginLeft: "auto", flexDirection: "row", gap: 10 }}>
           <MaterialCommunityIcons
             name="trophy-outline"
             size={19}
             color="black"
+            style={styles.iconBox}
           />
           <MaterialCommunityIcons
             name="share-outline"
             size={19}
             color="black"
+            style={styles.iconBox}
           />
         </View>
       </View>
@@ -84,5 +112,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 17,
     letterSpacing: 0.5,
+  },
+  iconBox: {
+    borderWidth: 0.5,
+    borderColor: "#D4D4D4",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
   },
 });

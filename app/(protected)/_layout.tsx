0b3556d5870a666de61bcb34c34dd33f2caf1c30
hrithikhadka <hrithikhadka00@gmail.com>
2025-10-28 21:48:19 +0200

    complete header of detailed post

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from "@clerk/clerk-expo";
-import { Redirect, Stack } from "expo-router";
+import { AntDesign, Entypo, MaterialIcons } from "@expo/vector-icons";
+import { Redirect, Stack, router } from "expo-router";
+import { View } from "react-native";
 
 export default function AuthRoutesLayout() {
   const { isSignedIn } = useAuth();
@@ -11,6 +13,29 @@ export default function AuthRoutesLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="post/[id]"
+        options={{
+          headerTitle: "",
+          headerStyle: { backgroundColor: "#FF5700" },
+          headerLeft: () => (
+            <AntDesign
+              name="close"
+              size={24}
+              color="white"
+              onPress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <AntDesign name="search1" size={24} color="white" />
+              <MaterialIcons name="sort" size={27} color="white" />
+              <Entypo name="dots-three-horizontal" size={24} color="white" />
+            </View>
+          ),
+          animation: "slide_from_bottom",
+        }}
+      />
     </Stack>
   );
 }

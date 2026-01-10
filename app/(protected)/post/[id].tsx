905600bfb57e1fa05c080ad31811d0369f7c7a1b
hrithikhadka <hrithikhadka00@gmail.com>
2026-01-10 22:13:51 +0200

    render comments on post detail

--- app/(protected)/post/[id].tsx
+++ app/(protected)/post/[id].tsx
@@ -1,6 +1,8 @@
+import CommentListItem from "@/app/components/CommentListItem";
 import PostListItem from "@/app/components/PostListItem";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import comments from "../../../assets/data/comments.json";
 import posts from "../../../assets/data/posts.json";
 
 const DetailedPost = () => {
@@ -8,12 +10,24 @@ const DetailedPost = () => {
 
   const detailedPost = posts.find((post) => post.id === id);
 
+  const postComments = comments.filter(
+    (comment) => comment.post_id === "post-1"
+  );
+
+  console.log(postComments);
+
   if (!detailedPost) {
     return <Text>Post not found</Text>;
   }
   return (
     <View>
-      <PostListItem post={detailedPost} isDetailedPost />
+      <FlatList
+        data={postComments}
+        renderItem={({ item }) => <CommentListItem comment={item} />}
+        ListHeaderComponent={
+          <PostListItem post={detailedPost} isDetailedPost />
+        }
+      />
     </View>
   );
 };

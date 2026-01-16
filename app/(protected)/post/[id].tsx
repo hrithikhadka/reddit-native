53fca2b5e0e09c9a45a3e78465308b400c72f577
hrithikhadka <hrithikhadka00@gmail.com>
2026-01-16 22:04:28 +0200

    add reply input and prevent keyboard from hiding it

--- app/(protected)/post/[id].tsx
+++ app/(protected)/post/[id].tsx
@@ -1,12 +1,25 @@
 import CommentListItem from "@/app/components/CommentListItem";
 import PostListItem from "@/app/components/PostListItem";
 import { useLocalSearchParams } from "expo-router";
-import { FlatList, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import comments from "../../../assets/data/comments.json";
 import posts from "../../../assets/data/posts.json";
 
 const DetailedPost = () => {
   const { id } = useLocalSearchParams();
+  const [comment, setComment] = useState<string>("");
+
+  const insets = useSafeAreaInsets();
 
   const detailedPost = posts.find((post) => post.id === id);
 
@@ -20,7 +33,11 @@ const DetailedPost = () => {
     return <Text>Post not found</Text>;
   }
   return (
-    <View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={insets.top}
+    >
       <FlatList
         data={postComments}
         renderItem={({ item }) => <CommentListItem comment={item} />}
@@ -28,7 +45,50 @@ const DetailedPost = () => {
           <PostListItem post={detailedPost} isDetailedPost />
         }
       />
-    </View>
+
+      <View
+        style={{
+          paddingBottom: insets.bottom,
+          borderBottomColor: "lightgray",
+          padding: 10,
+          backgroundColor: "white",
+          borderRadius: 10,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: -3 },
+          shadowOpacity: 0.1,
+          shadowRadius: 3,
+          elevation: 4,
+        }}
+      >
+        <TextInput
+          placeholder="Join the conversation"
+          style={{ backgroundColor: "#E4E4E4", padding: 5, borderRadius: 5 }}
+          value={comment}
+          onChangeText={(text) => setComment(text)}
+          multiline
+        />
+        <Pressable
+          style={{
+            backgroundColor: "#0d469b",
+            borderRadius: 15,
+            marginLeft: "auto",
+            marginTop: 15,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              paddingVertical: 5,
+              paddingHorizontal: 10,
+              fontWeight: "bold",
+              fontSize: 13,
+            }}
+          >
+            Reply
+          </Text>
+        </Pressable>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 export default DetailedPost;
